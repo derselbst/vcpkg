4f855ef848104f93def010e68452c4163ca39e5b
derselbst <tom.mbrt@googlemail.com>
2022-06-20 21:34:26 +0200

    [kf5archive] Fix build on Windows

--- ports/kf5archive/portfile.cmake
+++ ports/kf5archive/portfile.cmake
@@ -21,7 +21,6 @@ file(WRITE "${SOURCE_PATH}/.clang-format" "DisableFormat: true\nSortIncludes: fa
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DCMAKE_DISABLE_FIND_PACKAGE_PkgConfig=ON
         -DBUILD_TESTING=OFF
         -DBUILD_WITH_QT6=ON
         -DEXCLUDE_DEPRECATED_BEFORE_AND_AT=5.94.0
